a6069fc2802b2a15b1b61ebfef0b961e5ca6824e
AmanDeep9925 <amandeep58257@gmail.com>
2019-03-17 23:09:01 +0530

    Sorting Algos

---
+++ SortingAlgo/bubbleSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "myalgo.h"
+using namespace std;
+
+void bubSort(int arr[], int n) {
+	// Sorting of the array
+	// Comparing  adjacent elements
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n - i - 1; ++j) {
+			// checking the adjacent element
+			if (arr[j] > arr[j + 1])
+				// swap their position
+				swap(arr[j], arr[j + 1]);
+		}
+	}
+}
+
+int main() {
+
+	int arr[100], size;
+
+	cout << "Enter the size of the array : ";
+	cin >> size;
+
+	cout << "Enter the element of the array:";
+
+	inputArr(arr, size);
+
+	cout << "The unsorted array";
+
+	outputArr(arr, size);
+
+	bubSort(arr, size);
+
+	cout << "The sorted array:";
+
+	outputArr(arr, size);
+
+}

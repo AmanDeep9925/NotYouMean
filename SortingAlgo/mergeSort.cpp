1839ec01c6b739b18cb129df00d7e1da350507ff
AmanDeep9925 <amandeep58257@gmail.com>
2019-03-22 20:48:24 +0530

    Merge_Sort Algorithm

--- SortingAlgo/mergeSort.cpp
+++ SortingAlgo/mergeSort.cpp
@@ -1,24 +1,63 @@
 #include <iostream>
+#include "myio.h"
 using namespace std;
 
 void mergeArray(int arr[], int beg, int mid, int end) {
 	int p = beg;
 	int q = mid + 1;
 
-	int Arr[end - beg + 1],k;
+	int Arr[end - beg + 1], k=0;
 
-	for(int i=beg;i<end;++i){
-		if(p < mid){
+	for (int i = beg; i <= end; ++i) {
+		if (p > mid) {
 			Arr[k++] = arr[q++];
 		}
-		else if(q > end){
+		else if (q > end) {
 			Arr[k++] = arr[p++];
 		}
-		else if(Arr[p] < Arr[q]){
+		else if (arr[p] < arr[q]) {
 			Arr[k++] = arr[p++];
 		}
-		else{
+		else {
 			Arr[k++] = arr[q++];
 		}
 	}
+
+	for (int p = 0; p < k; ++p) {
+		arr[beg++] = Arr[p];
+	}
+}
+
+void mergeSort(int arr[], int beg, int end) {
+	if (beg < end) {
+		int mid = (beg + end) / 2;
+
+		mergeSort(arr,beg,mid);
+		mergeSort(arr,mid+1,end);
+		mergeArray(arr,beg,mid,end);
+	}
+}
+
+int main(){
+
+	int arr[100],size;
+
+	cout << "Enter the size of the array : ";
+	cin >> size;
+
+	int beg = 0,end = size-1;
+
+	cout << "Enter the element of the array:";
+
+	inputArr(arr, size);
+
+	cout << "The unsorted array : ";
+
+	outputArr(arr, size);
+
+	mergeSort(arr,beg,end);
+
+	cout << "The sorted array:";
+
+	outputArr(arr, size);
 }
